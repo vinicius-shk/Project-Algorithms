d21ee95519091212f8d44f158d05affdf44acfc3
NoteVini <vinicius.shk@gmail.com>
2023-03-22 17:01:11 -0300

    test: Implements a test for encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(None, 1)
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('None', 'B')
+
+    reverse = encrypt_message('batata', 99)
+    assert reverse == 'atatab'
+
+    even = encrypt_message('abcdef', 4)
+    assert even == 'fe_dcba'
+
+    odd = encrypt_message('abcdef', 3)
+    assert odd == 'cba_fed'
