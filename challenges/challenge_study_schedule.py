46cf2feccb4374ef5ce9e25a0bf0c933b210dd54
NoteVini <vinicius.shk@gmail.com>
2023-03-22 16:01:28 -0300

    feat: Implements study_schedule method to verify the number of students in a given target_time.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    student_numbers = 0
+    try: 
+        for in_time, out_time in permanence_period:
+            if in_time <= target_time <= out_time:
+                student_numbers += 1
+        return student_numbers
+    except TypeError:
+        return None
