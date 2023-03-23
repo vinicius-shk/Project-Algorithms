5fc5ab4b15ac3bd2d747918e91ad4fe9fc1732f6
NoteVini <vinicius.shk@gmail.com>
2023-03-23 17:42:38 -0300

    feat: Implements a find_duplicate method to sort out an list of numbers and return the first duplicate found.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,6 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums: list = [1]):
+    nums.sort()
+    for index, num in enumerate(nums[1::]):
+        if num == nums[index] and num > 0:
+            return num
+    return False
