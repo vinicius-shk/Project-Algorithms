7d0349b62794e00cc3d58ab38f998b9f5d7af82f
NoteVini <vinicius.shk@gmail.com>
2023-03-23 18:06:46 -0300

    feat: Implements a is_palindrome_iterative func to assert if a given word is a palindrome in a iteractive method.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,5 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not len(word):
+        return False
+    inverted = ''.join([*word[::-1]])
+    return word == inverted
